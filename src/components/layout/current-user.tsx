09060c531411ba0305fbc8a75b81795710a1d907
Steven Tardiff <tardiff.steven@gmail.com>
2024-01-19 15:03:40 -0500

    2nd commit

---
+++ src/components/layout/current-user.tsx
@@ -0,0 +1,18 @@
+import { Popover } from 'antd'
+
+const CurrentUser = () => {
+  return (
+    <>
+        <Popover 
+            placement='bottomRight'
+            trigger="click"
+            overlayInnerStyle={{padding:0}}
+            overlayStyle={{zIndex: 999}}
+        >
+            TEST
+        </Popover>
+    </>
+  )
+}
+
+export default CurrentUser
